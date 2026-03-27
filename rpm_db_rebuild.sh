a626c28a864a152eb53e29baf3151bf834e1370b
Daniel Lloyd <dnlloyd@outlook.com>
2026-03-27 10:30:34 -0500

    update rpm_db_rebuild.sh

--- rpm_db_rebuild.sh
+++ rpm_db_rebuild.sh
@@ -2,16 +2,16 @@
 
 # Rebuild the RPM database
 
-sudo ls -la /var/lib/rpm/__db* 2>/dev/null || true
-sudo rm -f /var/lib/rpm/__db* 2>/dev/null || true
+ls -la /var/lib/rpm/__db* 2>/dev/null || true
+rm -f /var/lib/rpm/__db* 2>/dev/null || true
 
 # 
 
-sudo mkdir -p /var/lib/rpm/backup.$(date +%F_%H%M%S)
-sudo cp -a /var/lib/rpm/* /var/lib/rpm/backup.$(date +%F_%H%M%S)/
+mkdir -p /var/lib/rpm/backup.$(date +%F_%H%M%S)
+cp -a /var/lib/rpm/* /var/lib/rpm/backup.$(date +%F_%H%M%S)/
 
-sudo rpm --rebuilddb
+rpm --rebuilddb
 
 # 
 
-sudo db_verify /var/lib/rpm/Packages 2>/dev/null || true
+db_verify /var/lib/rpm/Packages 2>/dev/null || true
